4f80cc59f1cac8b5970e3a9adc7b168d3cd4596c
theknzz <joaquimpersonalmail@isec.pt>
2020-10-26 12:28:42 +0000

    Updates

---
+++ precache-manifest.0bfe1d9db8a5aba233e1ee88d67674cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ebf072cc0abf4593f2a8c99e0027132",
+    "url": "/photo-gallery/index.html"
+  },
+  {
+    "revision": "23a0a3d0eaf6a7f59c86",
+    "url": "/photo-gallery/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "55b1d228f108e89b1f8b",
+    "url": "/photo-gallery/static/js/2.85d03e56.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/photo-gallery/static/js/2.85d03e56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23a0a3d0eaf6a7f59c86",
+    "url": "/photo-gallery/static/js/main.f21c403b.chunk.js"
+  },
+  {
+    "revision": "0e17d2a0b3a704a28038",
+    "url": "/photo-gallery/static/js/runtime-main.d9f48ce9.js"
+  }
+]);
